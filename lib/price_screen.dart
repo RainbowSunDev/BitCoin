e0dc5a1a3bafffcdac8c952fb5945f96d76dcea1
thsblue <thsbluehsg@gmail.com>
2019-12-03 07:56:40 +0530

    DropdownButton with limited no of items

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import './coin_data.dart';
+
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCur='USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +46,30 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCur,
+              items: [
+                DropdownMenuItem(
+                  child: Text("USD"),
+                  value: 'USD',
+                ),
+                DropdownMenuItem(
+                  child: Text("EUR"),
+                  value: 'EUR',
+                ),
+                DropdownMenuItem(
+                  child: Text("GBP"),
+                  value: 'GBP',
+                ),
+
+              ],
+              onChanged: (value){
+                setState(() {
+                  selectedCur=value;
+                });
+
+              },
+            ),
           ),
         ],
       ),
